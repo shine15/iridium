a01a29fd9a49ff58542c525b76f8e4fc5fc3888a
Evan <super.iridium77@gmail.com>
2021-01-25 16:33:34 +1100

    chart time axis in progress

--- iridium/chart/trade_chart.py
+++ iridium/chart/trade_chart.py
@@ -1,18 +1,20 @@
-import os
 from datetime import datetime
+import bisect
 from sys import platform as sys_pf
 import numpy as np
 import pandas as pd
+
+
 if sys_pf == 'darwin':
     import matplotlib
+
     matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
-import matplotlib.style as mplstyle
 from mpl_finance import candlestick2_ohlc
 import matplotlib.dates as mdates
 from matplotlib.ticker import Formatter, MaxNLocator
-from ..data.hdf5 import HDFData, FILE_PATH
-from ..utils.trading_calendar import DataFrequency
+from iridium.data.hdf5 import HDFData, FILE_PATH
+from iridium.utils.trading_calendar import DataFrequency
 
 
 class TradeDateFormatter(Formatter):
@@ -28,29 +30,7 @@ class TradeDateFormatter(Formatter):
         return mdates.num2date(self.dates[ind]).strftime(self.fmt)
 
 
-def _chart_data(instrument, frequency, start, end, start_offset=0, end_offset=0, file_path=FILE_PATH):
-    chart_start = pd.Timestamp(start, unit='s') - pd.Timedelta(start_offset * DataFrequency[frequency].value, unit='s')
-    chart_end = pd.Timestamp(end, unit='s') + pd.Timedelta(end_offset * DataFrequency[frequency].value, unit='s')
-    return HDFData.read_hdf(instrument, frequency, chart_start, chart_end, file_path)
-
-
-def draw_candlestick_chart(instrument, frequency, start, end, start_offset=0, end_offset=0, file_path=FILE_PATH):
-    """
-    Prepare data for chart
-    :param instrument: instrument name, string
-    :param frequency:
-    M* - Minute, H - hour, D - day, W - Week, M - Month
-    M1, M2, M4, M5, M10, M15, M30
-    H1, H2, H3, H4, H6, H8, H12,
-    D, W
-    :param start: timestamp
-    :param end: timestamp
-    :param start_offset: int
-    :param end_offset: int
-    :param file_path: HDF5 file path
-    :return: pandas DataFrame
-    """
-    df = _chart_data(instrument, frequency, start, end, start_offset, end_offset, file_path)
+def draw_candlestick_chart(df):
     fig, ax = plt.subplots(1, 1, sharex=True, figsize=(10, 5))
     line_width = 0.6
     date_list = [mdates.date2num(datetime.fromtimestamp(date.timestamp())) for date in df.index]
@@ -70,3 +50,64 @@ def draw_candlestick_chart(instrument, frequency, start, end, start_offset=0, en
         alpha=1.0)
     formatter = TradeDateFormatter(date_list)
     ax.xaxis.set_major_formatter(formatter)
+    return fig, ax
+
+
+def add_annotate(ax, message, x, y, x_offset=15, y_offset=15):
+    ax.annotate(
+        message,
+        xy=(x, y),
+        xytext=(x_offset, y_offset),
+        textcoords='offset points',
+        arrowprops=dict(arrowstyle="->", connectionstyle="arc3"),
+        color="black"
+    )
+
+
+def chart_data(instrument, frequency, start, end, start_offset=0, end_offset=0, file_path=FILE_PATH):
+    """
+    Prepare data for chart
+    :param instrument: instrument name, string
+    :param frequency:
+    M* - Minute, H - hour, D - day, W - Week, M - Month
+    M1, M2, M4, M5, M10, M15, M30
+    H1, H2, H3, H4, H6, H8, H12,
+    D, W
+    :param start: timestamp
+    :param end: timestamp
+    :param start_offset: int
+    :param end_offset: int
+    :param file_path: HDF5 file path
+    :return: pandas DataFrame
+    """
+    chart_start = pd.Timestamp(start, unit='s') - pd.Timedelta(start_offset * DataFrequency[frequency].value, unit='s')
+    chart_end = pd.Timestamp(end, unit='s') + pd.Timedelta(end_offset * DataFrequency[frequency].value, unit='s')
+    return HDFData.read_hdf(instrument, frequency, chart_start, chart_end, file_path)
+
+
+def date_time_index(df, date_time):
+    """
+    find time index for chart
+    :param df: pandas data frame
+    :param date_time: timestamp
+    :return: time index
+    """
+    dt = pd.Timestamp(date_time, unit='s')
+    idx = np.searchsorted(df.index.asi8, dt.value, side="left")
+    return idx
+
+
+if __name__ == "__main__":
+    instrument = "EUR_USD"
+    freq = "D"
+    start = 1548108060
+    end = 1548339840
+    start_offset = 0
+    end_offset = 0
+    df = chart_data(instrument, freq, start, end, start_offset, end_offset)
+    fig, ax = draw_candlestick_chart(df)
+    x1 = date_time_index(df, start)
+    y1 = df.close[x1]
+    add_annotate(ax, y1, x1, y1)
+
+    plt.show()
